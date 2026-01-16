047c655169a613c2bad692d9c4ed5d5a238cc04a
eylul-kilinc <eylulkilinc34@gmail.com>
2026-01-16 20:35:57 +0300

    update text

--- eylul-kilinc-portfolio/app/page.tsx
+++ eylul-kilinc-portfolio/app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
         <div className="flex flex-col items-center gap-6 text-center">
           <h1 className="text-4xl font-semibold leading-tight tracking-tight text-black dark:text-zinc-50">
-             One can not imagine a world without her.
+             
             
           </h1>
           <h1 className="text-4xl font-semibold leading-tight tracking-tight text-black dark:text-zinc-50">
-             One can not imagine how much umut loves eylul kilinc.
+             
             
           </h1>
         
